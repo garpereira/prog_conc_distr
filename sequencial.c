977ea7628402f5667a4702dd53605db3a4836545
garpereira <garpereira@unifesp.br>
2024-12-01 10:47:34 -0300

    :recycle: refactor: Atualização da versão sequencial, adicionado uma redução

--- sequencial.c
+++ sequencial.c
@@ -1,12 +1,14 @@
-# include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-# define N 100  // Tamanho da grade
-# define T 1000 // Número de iterações
-# define D 0.1  // Coeficiente de difusão
-# define DELTA_T 0.01
-# define DELTA_X 1.0
+#define N 2000 // Tamanho da grade
+#define T 500 // Número de iterações no tempo
+#define D 0.1 // Coeficiente de difusão
+#define DELTA_T 0.01
+#define DELTA_X 1.0
 
-void diff_eq(double C[N][N], double C_new[N][N]) {
+void diff_eq(double **C, double **C_new) { //diff_eq(double C[N][N], double C_new[N][N]) {
     for (int t = 0; t < T; t++) {
         for (int i = 1; i < N - 1; i++) {
             for (int j = 1; j < N - 1; j++) {
@@ -16,24 +18,59 @@ void diff_eq(double C[N][N], double C_new[N][N]) {
             }
         }
         // Atualizar matriz para a próxima iteração
+        double difmedio = 0.;
         for (int i = 1; i < N - 1; i++) {
             for (int j = 1; j < N - 1; j++) {
+                difmedio += fabs(C_new[i][j] - C[i][j]);
                 C[i][j] = C_new[i][j];
             }
         }
+        if ((t%100) == 0)
+            printf("interacao %d - diferenca=%g\n", t, difmedio/((N-2)*(N-2)));
     }
 }
 
 int main() {
-    double C[N][N] = {0};      // Concentração inicial
-    double C_new[N][N] = {0};  // Concentração para a próxima iteração
-
+    // Concentração inicial
+    double **C = (double **)malloc(N * sizeof(double *));
+    if (C == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return 1;
+    }
+    for (int i = 0; i < N; i++) {
+        C[i] = (double *)malloc(N * sizeof(double));
+        if (C[i] == NULL) {
+            fprintf(stderr, "Memory allocation failed\n");
+            return 1;
+        }
+    }
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            C[i][j] = 0.;
+        }
+    }
+    // Concentração para a próxima iteração
+    double **C_new = (double **)malloc(N * sizeof(double *));
+    if (C_new == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return 1;
+    }
+    for (int i = 0; i < N; i++) {
+        C_new[i] = (double *)malloc(N * sizeof(double));
+        if (C_new[i] == NULL) {
+            fprintf(stderr, "Memory allocation failed\n");
+            return 1;
+        }
+    }
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            C_new[i][j] = 0.;
+        }
+    }
     // Inicializar uma concentração alta no centro
     C[N/2][N/2] = 1.0;
-
-    // Executar a equação de difusão
+    // Executar as iterações no tempo para a equação de difusão
     diff_eq(C, C_new);
-
     // Exibir resultado para verificação
     printf("Concentração final no centro: %f\n", C[N/2][N/2]);
     return 0;
